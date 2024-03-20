bd53706e83f838d82846535eeb403005f94acd22
Paspatate <82947263+Paspatate@users.noreply.github.com>
2024-03-20 15:45:30 +0100

    ajout de la liste des rover

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const view = {
+    roverList: document.getElementById("rover-list"),
+};
+
+const API_ENDPOINT = "https://mars-photos.herokuapp.com/api/v1";
+
+let showRovers = function(rovers) {
+    for (const rover of rovers) {
+        let list_elem = document.createElement("li");
+        
+        let input_elem = document.createElement("input");
+        input_elem.type = "checkbox";
+        input_elem.value = rover.name;
+        input_elem.id = rover.id;
+
+        let checkbox_label_elem = document.createElement("label");
+        checkbox_label_elem.setAttribute("for", rover.id);
+        checkbox_label_elem.innerText = rover.name;
+
+        list_elem.appendChild(input_elem);
+        list_elem.appendChild(checkbox_label_elem);
+
+        view.roverList.appendChild(list_elem);
+    }
+
+};
+
+fetch(API_ENDPOINT + "/rovers")
+    .then(res => res.json())
+    .then(res => {
+        console.log(res);
+        showRovers(res.rovers);
+    }).catch((err) => {console.error("Error when loading rover"); console.error(err)});
